adf595e065536f5f65594c7fbf3c9e50a8d38cec
bedirhansahin <bedirhannsahin@gmail.com>
2023-04-12 18:23:02 +0300

    User admin page is configured

--- app/core/admin.py
+++ app/core/admin.py
@@ -2,4 +2,12 @@ from django.contrib import admin
 from . models import *
 
 
-admin.site.register(User)
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['username', 'first_name', 'last_name', 'gender', 'birth_date', 'is_active']
+    search_fields = ['username']
+    search_help_text = "You can search by username "
+    list_filter = ['gender', 'is_active', 'is_staff']
+    list_per_page = 10
+    ordering = ['date_joined']
+    readonly_fields = ['username', 'email']
--- app/core/models_choices.py
+++ app/core/models_choices.py
@@ -1,6 +1,14 @@
+from datetime import datetime
+
 # To select a gender
 GENDER_CHOICES = (
     ('M', 'MALE'),
     ('F', 'FEMALE'),
     ('O', 'OTHER')
 )
+
+
+# To select a year
+YEAR_CHOICES = []
+for i in range(1890, datetime.now().year+1):
+    YEAR_CHOICES.append(i)
