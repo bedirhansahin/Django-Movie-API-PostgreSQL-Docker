03a615bffcef4e92553568f4afb155ccbe7ee0e3
bedirhansahin <bedirhannsahin@gmail.com>
2023-04-12 17:20:02 +0300

    User model changed

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+from . models_choices import *
+
+
+class User(AbstractUser):
+    birth_date = models.DateField(_("birth date"), null=True, blank=True)
+    gender = models.CharField(_("gender"), max_length=1, choices=GENDER_CHOICES, null=True, blank=True)
+    REQUIRED_FIELDS = ["email"]
+
+    def __str__(self):
+        return self.username
+
---
+++ app/core/models_choices.py
@@ -0,0 +1,6 @@
+# To select a gender
+GENDER_CHOICES = (
+    ('M', 'MALE'),
+    ('F', 'FEMALE'),
+    ('O', 'OTHER')
+)
